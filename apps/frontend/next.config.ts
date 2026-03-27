23d7d2a6ab489e97f8d7aa2b555981ed78f3b448
miguelzvd <dilsonmiguel25@gmail.com>
2026-03-27 00:01:53 -0300

    feat(frontend): implementar resto de configuração e  e dockerização do front end

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: "standalone",
+  transpilePackages: ["@field-report/shared"],
+}
 
-export default nextConfig;
+export default nextConfig
